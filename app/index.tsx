831feaa6c1ef7f876658c338e4fa181c56385a4d
YongTang <delicious50up55@gmail.com>
2025-12-04 03:23:52 +0900

    [fix/login]: 로그인 임시 적용

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,11 @@
+import { useAuth } from "@/contexts/AuthContext";
 import { Redirect } from "expo-router";
+import React from "react";
 
 export default function Index() {
-  return <Redirect href="/login" />;
+  const { userToken, loading } = useAuth();
+
+  if (loading) return null; // 로딩 중 스플래시 화면 가능
+
+  return <Redirect href={userToken ? "/home" : "/login"} />;
 }
